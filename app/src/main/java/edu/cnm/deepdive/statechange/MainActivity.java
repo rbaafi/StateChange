9257a9e89d08a879e6f9612562d69f4849767762
Richmond <rbaafi@cnm.edu>
2020-01-26 14:33:32 -0700

    Completed StateChange excercise.

---
+++ app/src/main/java/edu/cnm/deepdive/statechange/MainActivity.java
@@ -0,0 +1,106 @@
+package edu.cnm.deepdive.statechange;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = "StateChange";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    FloatingActionButton fab = findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
+
+    Log.i(TAG, "ONcREATE");
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.i(TAG, "onStart");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.i(TAG, "onResume");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.i(TAG, "onPause");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.i(TAG, "onStop");
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    Log.i(TAG, "onRestart");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.i(TAG, "onDestroy");
+  }
+
+  @Override
+  protected void onSaveInstanceState(@NonNull Bundle outState) {
+    super.onSaveInstanceState(outState);
+    Log.i(TAG, "onSaveInstanceState");
+  }
+
+  @Override
+  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    Log.i(TAG, "onRestoreInstanceState");
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
